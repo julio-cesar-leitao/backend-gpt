b4eebadb61e276b64bd723fa0897066f8e9fbe31
Julio Cesar <juliocesar@MacBook-Pro-de-Julio.local>
2023-08-24 15:14:30 -0300

    feat: initial code

---
+++ backend-gpt/src/main/java/br/com/juliocesarleitao/backendgpt/BackendGptApplication.java
@@ -0,0 +1,13 @@
+package br.com.juliocesarleitao.backendgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGptApplication.class, args);
+	}
+
+}
---
+++ backend-gpt/src/test/java/br/com/juliocesarleitao/backendgpt/BackendGptApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juliocesarleitao.backendgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
